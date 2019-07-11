125461b2194c4fc9f2e5e18beb2908ef31f3a8ba
Matteo Corain <m96.corain@gmail.com>
2019-07-11 17:18:01 +0200

    Lab #9: added exercise 3

---
+++ lab09/ex03/src/main/java/it/polito/bigdata/lab09/ex03/FineFoodAnalysis.java
@@ -0,0 +1,109 @@
+package it.polito.bigdata.lab09.ex03;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.classification.LogisticRegression;
+import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.mllib.evaluation.MulticlassMetrics;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.ml.feature.LabeledPoint;
+
+public class FineFoodAnalysis {
+
+    private static final Double THRESHOLD = 0.9;
+
+    public static void main(String[] args) {
+        // Parse command-line parameters
+        String inputPath = args[0];
+
+        // Create the Spark session
+        SparkSession ss = SparkSession.builder()
+            .appName("Lab 9.3 - Classification on the Amazon fine-foods dataset (multiple features)")
+            .getOrCreate();
+        
+        // Create the Spark context
+        JavaSparkContext sc = new JavaSparkContext(ss.sparkContext());
+
+        // Create the input dataframe
+        Dataset<Row> reviewsDataframe = ss.read()
+            .format("csv")
+            .option("delimiter", ",")
+            .option("inferSchema", true)
+            .option("header", true)
+            .load(inputPath);
+
+        // Create the labeled points RDD
+        JavaRDD<LabeledPoint> labeledPointsRDD = reviewsDataframe
+            .toJavaRDD()
+            .filter(r -> r.getInt(5) != 0)
+            .map(r -> {
+                // Get the review text, summary and score
+                String text = r.getString(9);
+                String summary = r.getString(8);
+                Integer score = r.getInt(6);
+                
+                // Compute measures
+                Integer length = text.length();
+                Integer numberOfWords = text.split(" ").length;
+                Integer summaryLength = summary.length();
+                Integer summaryNumberOfWords = summary.split(" ").length;
+
+                // Compute the helpfulness index
+                Double helpfulness = (double) r.getInt(4) / r.getInt(5);
+
+                // Emit the labeled point
+                return new LabeledPoint(helpfulness < THRESHOLD ? 0.0 : 1.0, Vectors.dense(
+                    length, numberOfWords, summaryLength, summaryNumberOfWords, score));
+            });
+        
+        // Create the labeled points Dataframe
+        Dataset<Row> labeledPointsDataframe = ss.createDataFrame(labeledPointsRDD, LabeledPoint.class);
+        
+        // Display 5 example rows
+        labeledPointsDataframe.show(5);
+        
+        // Split the data into training and test sets (30% held out for testing)
+        Dataset<Row>[] splits = labeledPointsDataframe.randomSplit(new double[]{ 0.7, 0.3 });
+        Dataset<Row> trainingData = splits[0].cache();
+        Dataset<Row> testData = splits[1].cache();
+
+        // Define the classification algorithm
+        LogisticRegression lr = new LogisticRegression();
+        lr.setMaxIter(10);
+        lr.setRegParam(0.01);
+
+        // Create the pipeline
+        Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{ lr });
+        
+        // Train the model
+        PipelineModel model = pipeline.fit(trainingData);
+        
+        // Apply the model to the test set
+        Dataset<Row> predictions = model.transform(testData);
+
+        // Compute the quality metrics
+        MulticlassMetrics metrics = new MulticlassMetrics(predictions.select("prediction", "label"));
+
+        // Show the confusion matrix
+        Matrix confusion = metrics.confusionMatrix();
+        System.out.println("Confusion matrix: \n" + confusion);
+
+        // Show the overall accuracy
+        double accuracy = metrics.accuracy();
+        System.out.println("Accuracy = " + accuracy);
+            
+        // Close the Spark context
+        sc.close();
+        
+        // Stop the Spark session
+        ss.stop();
+        ss.close();
+    }
+}
